25f1b613f6df0ac853e0230d5ab5385ef5600110
TheThunderGuyS <saihaj.sran@gmail.com>
2020-06-17 10:02:21 -0400

    console.log(delet.length);
    codefactor may shut up now

--- media/bruh.js
+++ media/bruh.js
@@ -1,18 +1,19 @@
 function delet() {
     window.alert("bruh");
     console.warn("bruh");
-    const bruh = new Audio("media/bruh.mp3");
+    var bruh = new Audio("media/bruh.mp3");
     bruh.play();
     document.write("<h1>bruh</h1><button>ok</button><br><h2>Refresh ur page</h2>");
-    const fgfg = window.open("", "", "width=400, height=200");
+    var fgfg = window.open("", "", "width=400, height=200");
     fgfg.focus();
-    fgfg.document.write("<h1>bruh</h1><button>ok</button><br><h2>Refresh ur page</h2>");
-    function bruandom(max) {
-        return ~~(Math.random() * ~~(max));
-    }
-    let i;
+    fgfg.document.write("<h1>bruh</h1><button>ok</button><br><h2>clos this popup andRefresh ur page</h2>");
+    var i;
     for (i = 0; i < 420; i++) {
         fgfg.moveBy(bruandom(250),bruandom(250));
         fgfg.resizeBy(bruandom(250),bruandom(250));
     }
 }
+function bruandom(max) {
+    return ~~(Math.random() * ~~(max));
+}
+console.log(delet.length); //Are you fcuking happy now CodeFactor?
